cda45ce9de0082dc5f9974e4fa7150ba3eb201d9
zhengyueling1993 <hxq4940@163.com>
2016-04-12 00:08:37 +0800

    Code/

---
+++ Code/ip4_address_conversion.py
@@ -0,0 +1,10 @@
+import socket
+from binascii import hexlify
+def convert_ip4_address():
+	for ip_addr in['127.0.0.1', '192.168.0.1']：
+		packed_ip_addr = socket.inet_aton(ip_addr)
+		unpacked_ip_addr = socket.inet_aton(packed_ip_addr)
+		print "IP Address: %s => Packed: %s, Unpacked: %s"\
+	%(ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr)
+if __name__ == '__main__':
+	convert_ip4_address()
