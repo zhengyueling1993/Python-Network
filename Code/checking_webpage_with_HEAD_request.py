3819147acf90264e1c4f1b05d58681521f7fbf1e
zhengyueling1993 <hxq4940@163.com>
2016-04-17 00:27:54 +0800

    http_compression.py

---
+++ Code/checking_webpage_with_HEAD_request.py
@@ -0,0 +1,31 @@
+import argparse
+import urllib
+import urlparse
+import re
+import httplib
+
+DEFAULT_URL = 'http://www.python.org'
+HTTP_GOOD_CODES = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
+
+def get_server_status_code(url):
+     """
+     Download just the header of a URL and
+     return the server's status code.
+     """
+     host, path = urlparse.urlparse(url)[1:3]
+     try:
+        conn = httplib.HTTPConnection(host)
+        conn.request('HEAD', path)
+        return conn.getresponse().status
+     except StandardError:
+        return None
+
+if __name__ == '__main__':
+     parser = argparse.ArgumentParser(description='Example HEADRequest')
+     parser.add_argument('--url', action="store", dest="url",default=DEFAULT_URL)
+     given_args = parser.parse_args()
+     url = given_args.url
+     if get_server_status_code(url) in HTTP_GOOD_CODES:
+       print "Server: %s status is OK: " %url
+     else:
+        print "Server: %s status is NOT OK!" %url
